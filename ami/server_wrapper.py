c6b2a502c7cd771620f2bbcd4f8c96762c91e45f
Eric Jiang <erjiang@indiana.edu>
2020-01-14 22:23:40 -0800

    Download the latest server JAR on startup
    server_wrapper.py now tries to get Mojang's JSON feeds that list where
    to get the latest server JAR, and then downloads it, overwriting the JAR
    in /home/ubuntu/.

--- ami/server_wrapper.py
+++ ami/server_wrapper.py
@@ -1,4 +1,6 @@
+import json
 import subprocess
+import urllib.request
 from xmlrpc.server import SimpleXMLRPCServer
 
 # This should be run in the /home/ubuntu directory
@@ -11,7 +13,30 @@ SERVER_FLAGS = [
     "-XX:+AggressiveOpts",
 ]
 
+SERVER_JAR_PATH = "/home/ubuntu/server.jar"
+
 minecraft_server = None
+
+
+def download_json(url):
+    with urllib.request.urlopen(url) as json_feed:
+        return json.loads(json_feed.read().decode())
+
+
+def download_latest_server(out_path):
+    """Given an output path "out_path", download the latest Minecraft server
+    JAR and save it to out_path. Note that out_path needs to be the full path
+    and filename, e.g. "/tmp/minecraft_server.jar"."""
+    mc_versions = download_json("https://launchermeta.mojang.com/mc/game/version_manifest.json")
+    # This assumes the versions are listed from newest to oldest. If this is
+    # not the case, then we need to use mc_versions['latest'] instead.
+    latest_release = next((ver for ver in mc_versions['versions'] if ver['type'] == 'release'), None)
+    latest_metadata = download_json(latest_release['url'])
+    jar_url = latest_metadata['downloads']['server']['url']
+    with urllib.request.urlopen(jar_url) as server_jar_req:
+        with open(out_path, 'wb') as out_handle:
+            out_handle.write(server_jar_req.read())
+    return True
 
 
 def run_command(cmd):
@@ -24,16 +49,17 @@ def run_command(cmd):
     return "Success"
 
 
-def run_minecraft():
+def run_minecraft(server_jar_path):
     global minecraft_server
     minecraft_server = subprocess.Popen(
-        ["java", "-Xms1024M", "-Xmx3000M"] + SERVER_FLAGS + ["-jar", "/home/ubuntu/server.jar", "nogui"],
+        ["java", "-Xms1024M", "-Xmx3000M"] + SERVER_FLAGS + ["-jar", server_jar_path, "nogui"],
         stdin=subprocess.PIPE
     )
 
 
 def main():
-    run_minecraft()
+    download_latest_server(SERVER_JAR_PATH)
+    run_minecraft(SERVER_JAR_PATH)
 
     # Allow other processes on this machine to send commands to the Minecraft
     # server via XMLRPC. The automatic shutdown uses this to tell players on
